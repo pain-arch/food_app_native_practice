1d3959e52f67dac2e959ceac52c0d1e6d3fd2fe0
pain-arch <himulol123@gmail.com>
2025-01-27 20:19:04 +0600

    Implement cart functionality in CartScreen and enhance CartProvider to calculate total

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,14 +1,25 @@
-import { StatusBar } from 'expo-status-bar'
-import { View, Text, Platform } from 'react-native'
+import { StatusBar } from "expo-status-bar";
+import { View, Text, Platform, FlatList } from "react-native";
+import { useCart } from "../providers/CartProvider";
+import CartListItem from "../components/CartListItem";
+import Button from "../components/Button";
 
 const CartScreen = () => {
+  const { items, total} = useCart();
+  console.log(items);
+  
   return (
-    <View>
-      <Text>CartScreen</Text>
-
-      <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
+    <View style={{ padding: 10 }}>
+      <FlatList
+        data={items}
+        renderItem={({ item }) => <CartListItem cartItem={item} />}
+        contentContainerStyle={{ padding: 10 , gap: 10}}
+        />
+        <Text style={{ fontSize: 20, fontWeight: "bold", marginTop: 20 }}>Total: {total }</Text>
+        <Button text="Checkout" />
+      <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
     </View>
-  )
-}
+  );
+};
 
-export default CartScreen
+export default CartScreen;
